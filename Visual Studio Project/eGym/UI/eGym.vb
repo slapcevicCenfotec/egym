396df4ca28abb7079fb89305bbd9a9b758f99551
Melvin Salas <melvinsalasrivera@gmail.com>
2015-03-01 19:03:46 -0600

    Creación del formulario principal
    Se añadió las librerías de interfaz de Metro Framework y se creó el
    formulario principal

---
+++ Visual Studio Project/eGym/UI/eGym.vb
@@ -0,0 +1,26 @@
+﻿Public Class eGym
+
+    Private Sub eGym_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InicializarMenu()
+    End Sub
+
+    Private Sub InicializarMenu()
+        For Each lbl As Label In menuPanel.Controls.OfType(Of Label)()
+            AddHandler lbl.Click, Function(senderObj, args) Seleccionar(lbl)
+        Next
+    End Sub
+
+    Private Function Seleccionar(lbl As Label)
+        For Each ctr As Label In menuPanel.Controls.OfType(Of Label)()
+            ctr.BackColor = Color.White
+            ctr.ForeColor = Color.Black
+        Next
+        lbl.BackColor = Color.FromArgb(0, 170, 171)
+        lbl.ForeColor = Color.White
+        Return lbl
+    End Function
+
+    Private Sub btnUsuarios_Click(sender As Object, e As EventArgs) Handles btnUsuarios.Click
+
+    End Sub
+End Class
