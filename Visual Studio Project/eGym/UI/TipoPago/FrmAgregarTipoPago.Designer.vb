0be262b1f3ac25923b8d9c3555af52422af0c71f
tarrasque2501 <nyther2501@gmail.com>
2015-03-04 19:11:25 -0600

    FORMS:edicion de forms de tipo de pago

--- Visual Studio Project/eGym/UI/TipoPago/FrmAgregarTipoPago.Designer.vb
+++ Visual Studio Project/eGym/UI/TipoPago/FrmAgregarTipoPago.Designer.vb
@@ -22,8 +22,140 @@ Partial Class FrmAgregarTipoPago
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel2 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel3 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel4 = New MetroFramework.Controls.MetroLabel()
+        Me.MetroTextBox1 = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroTextBox2 = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroTextBox3 = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton2 = New MetroFramework.Controls.MetroButton()
+        Me.SuspendLayout()
+        '
+        'MetroLabel1
+        '
+        Me.MetroLabel1.AutoSize = True
+        Me.MetroLabel1.Location = New System.Drawing.Point(149, 25)
+        Me.MetroLabel1.Name = "MetroLabel1"
+        Me.MetroLabel1.Size = New System.Drawing.Size(62, 19)
+        Me.MetroLabel1.TabIndex = 0
+        Me.MetroLabel1.Text = "Nombre:"
+        '
+        'MetroLabel2
+        '
+        Me.MetroLabel2.AutoSize = True
+        Me.MetroLabel2.Location = New System.Drawing.Point(158, 68)
+        Me.MetroLabel2.Name = "MetroLabel2"
+        Me.MetroLabel2.Size = New System.Drawing.Size(51, 19)
+        Me.MetroLabel2.TabIndex = 1
+        Me.MetroLabel2.Text = "Monto:"
+        '
+        'MetroLabel3
+        '
+        Me.MetroLabel3.AutoSize = True
+        Me.MetroLabel3.Location = New System.Drawing.Point(144, 109)
+        Me.MetroLabel3.Name = "MetroLabel3"
+        Me.MetroLabel3.Size = New System.Drawing.Size(64, 19)
+        Me.MetroLabel3.TabIndex = 2
+        Me.MetroLabel3.Text = "Duracion:"
+        '
+        'MetroLabel4
+        '
+        Me.MetroLabel4.AutoSize = True
+        Me.MetroLabel4.Location = New System.Drawing.Point(0, 0)
+        Me.MetroLabel4.Name = "MetroLabel4"
+        Me.MetroLabel4.Size = New System.Drawing.Size(130, 19)
+        Me.MetroLabel4.TabIndex = 3
+        Me.MetroLabel4.Text = "Nuevo Tipo de Pago"
+        '
+        'MetroTextBox1
+        '
+        Me.MetroTextBox1.Lines = New String() {"MetroTextBox1"}
+        Me.MetroTextBox1.Location = New System.Drawing.Point(214, 25)
+        Me.MetroTextBox1.MaxLength = 32767
+        Me.MetroTextBox1.Name = "MetroTextBox1"
+        Me.MetroTextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.MetroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.MetroTextBox1.SelectedText = ""
+        Me.MetroTextBox1.Size = New System.Drawing.Size(144, 23)
+        Me.MetroTextBox1.TabIndex = 4
+        Me.MetroTextBox1.Text = "MetroTextBox1"
+        Me.MetroTextBox1.UseSelectable = True
+        '
+        'MetroTextBox2
+        '
+        Me.MetroTextBox2.Lines = New String() {"MetroTextBox2"}
+        Me.MetroTextBox2.Location = New System.Drawing.Point(214, 68)
+        Me.MetroTextBox2.MaxLength = 32767
+        Me.MetroTextBox2.Name = "MetroTextBox2"
+        Me.MetroTextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.MetroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.MetroTextBox2.SelectedText = ""
+        Me.MetroTextBox2.Size = New System.Drawing.Size(144, 23)
+        Me.MetroTextBox2.TabIndex = 5
+        Me.MetroTextBox2.Text = "MetroTextBox2"
+        Me.MetroTextBox2.UseSelectable = True
+        '
+        'MetroTextBox3
+        '
+        Me.MetroTextBox3.Lines = New String() {"MetroTextBox3"}
+        Me.MetroTextBox3.Location = New System.Drawing.Point(214, 109)
+        Me.MetroTextBox3.MaxLength = 32767
+        Me.MetroTextBox3.Name = "MetroTextBox3"
+        Me.MetroTextBox3.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.MetroTextBox3.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.MetroTextBox3.SelectedText = ""
+        Me.MetroTextBox3.Size = New System.Drawing.Size(144, 23)
+        Me.MetroTextBox3.TabIndex = 6
+        Me.MetroTextBox3.Text = "MetroTextBox3"
+        Me.MetroTextBox3.UseSelectable = True
+        '
+        'MetroButton1
+        '
+        Me.MetroButton1.Location = New System.Drawing.Point(144, 208)
+        Me.MetroButton1.Name = "MetroButton1"
+        Me.MetroButton1.Size = New System.Drawing.Size(75, 23)
+        Me.MetroButton1.TabIndex = 7
+        Me.MetroButton1.Text = "Confirmar"
+        Me.MetroButton1.UseSelectable = True
+        '
+        'MetroButton2
+        '
+        Me.MetroButton2.Location = New System.Drawing.Point(283, 208)
+        Me.MetroButton2.Name = "MetroButton2"
+        Me.MetroButton2.Size = New System.Drawing.Size(75, 23)
+        Me.MetroButton2.TabIndex = 8
+        Me.MetroButton2.Text = "Cancelar"
+        Me.MetroButton2.UseSelectable = True
+        '
+        'FrmAgregarTipoPago
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.MetroButton2)
+        Me.Controls.Add(Me.MetroButton1)
+        Me.Controls.Add(Me.MetroTextBox3)
+        Me.Controls.Add(Me.MetroTextBox2)
+        Me.Controls.Add(Me.MetroTextBox1)
+        Me.Controls.Add(Me.MetroLabel4)
+        Me.Controls.Add(Me.MetroLabel3)
+        Me.Controls.Add(Me.MetroLabel2)
+        Me.Controls.Add(Me.MetroLabel1)
+        Me.Name = "FrmAgregarTipoPago"
+        Me.Size = New System.Drawing.Size(554, 336)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents MetroLabel1 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel2 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel3 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel4 As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroTextBox1 As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroTextBox2 As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroTextBox3 As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroButton1 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton2 As MetroFramework.Controls.MetroButton
 
 End Class
--- Visual Studio Project/eGym/UI/TipoPago/FrmListarTipoPago.vb
+++ Visual Studio Project/eGym/UI/TipoPago/FrmListarTipoPago.vb
@@ -1,3 +1,18 @@
 ﻿Public Class FrmListarTipoPago
 
+    Private Sub MetroButton1_Click(sender As Object, e As EventArgs) Handles MetroButton1.Click
+        Dim ctr As Control
+        ctr = New FrmAgregarTipoPago
+        ctr.Dock = DockStyle.Fill
+        Me.Controls.Clear()
+        Me.Controls.Add(ctr)
+    End Sub
+
+    Private Sub MetroButton2_Click(sender As Object, e As EventArgs) Handles MetroButton2.Click
+        Dim ctr As Control
+        ctr = New FrmModificarTipoPago
+        ctr.Dock = DockStyle.Fill
+        Me.Controls.Clear()
+        Me.Controls.Add(ctr)
+    End Sub
 End Class
