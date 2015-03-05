da61f1fa3a5e55b999b1189b1cdf64b79bd45433
mauriciofer <mauriciofer@gmail.com>
2015-03-04 19:55:00 -0600

    Registrar tipo de maquina

---
+++ Visual Studio Project/eGym/DAL/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Save();
+        void Clear();
+    }
+}
