790edcba744ad795ea8bd5fdd45375ef09f0563e
tarrasque2501 <nyther2501@gmail.com>
2015-03-04 20:12:53 -0600

    IRespository,IEntity y demas

--- Visual Studio Project/eGym/DAL/Repositories/IRepository.cs
+++ Visual Studio Project/eGym/DAL/Repositories/IRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EL;
 
 namespace DAL.Repositories
 {
@@ -12,7 +13,6 @@ namespace DAL.Repositories
         void Delete(T entity);
         void Update(T entity);
         IEnumerable<T> GetAll();
-        ////T GetById(int id);
         void Save();
         void Clear();
     }
